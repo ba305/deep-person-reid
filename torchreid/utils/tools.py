d9ee52e4220d2bbcdefa9769f2469eae244d9cfe
Ben Alexander <ben@goradar.com>
2019-06-20 12:31:46 -0400

    add cudnn random seed

--- torchreid/utils/tools.py
+++ torchreid/utils/tools.py
@@ -65,6 +65,8 @@ def set_random_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def download_url(url, dst):
