0b5c1a2d5597d0e7aecc29835e4e0b0735cd8cb4
Ben Alexander <ben@goradar.com>
2019-08-05 17:40:04 -0400

    comment out deprecated code

--- torchreid/data/datamanager.py
+++ torchreid/data/datamanager.py
@@ -253,153 +253,156 @@ class ImageDataManager(DataManager):
         print('\n')
 
 
-class VideoDataManager(DataManager):
-    """Video data manager.
-
-    Args:
-        root (str): root path to datasets.
-        sources (str or list): source dataset(s).
-        targets (str or list, optional): target dataset(s). If not given,
-            it equals to ``sources``.
-        height (int, optional): target image height. Default is 256.
-        width (int, optional): target image width. Default is 128.
-        random_erase (bool, optional): use random erasing. Default is False.
-        color_jitter (bool, optional): use color jittering. Default is False.
-        color_aug (bool, optional): use color augmentation. Default is False.
-        use_cpu (bool, optional): use cpu. Default is False.
-        split_id (int, optional): split id (*0-based*). Default is 0.
-        combineall (bool, optional): combine train, query and gallery in a dataset for
-            training. Default is False.
-        batch_size (int, optional): number of *tracklets* in a batch. Default is 3.
-        workers (int, optional): number of workers. Default is 4.
-        num_instances (int, optional): number of instances per identity in a batch.
-            Default is 4.
-        train_sampler (str, optional): sampler. Default is empty (``RandomSampler``).
-        seq_len (int, optional): how many images to sample in a tracklet. Default is 15.
-        sample_method (str, optional): how to sample images in a tracklet. Default is "evenly".
-            Choices are ["evenly", "random", "all"]. "evenly" and "random" sample ``seq_len``
-            images in a tracklet while "all" samples all images in a tracklet, thus ``batch_size``
-            needs to be set to 1.
-
-    Examples::
-
-        datamanager = torchreid.data.VideoDataManager(
-            root='path/to/reid-data',
-            sources='mars',
-            height=256,
-            width=128,
-            batch_size=3,
-            seq_len=15,
-            sample_method='evenly'
-        )
-
-    .. note::
-        The current implementation only supports image-like training. Therefore, each image in a
-        sampled tracklet will undergo independent transformation functions. To achieve tracklet-aware
-        training, you need to modify the transformation functions for video reid such that each function
-        applies the same operation to all images in a tracklet to keep consistency.
-    """
-
-    def __init__(self, root='', sources=None, targets=None, height=256, width=128, random_erase=False,
-                 color_jitter=False, color_aug=False, use_cpu=False, split_id=0, combineall=False,
-                 batch_size=3, workers=4, num_instances=4, train_sampler=None,
-                 seq_len=15, sample_method='evenly'):
-
-        super(VideoDataManager, self).__init__(sources=sources, targets=targets, height=height, width=width,
-                                               random_erase=random_erase, color_jitter=color_jitter,
-                                               color_aug=color_aug, use_cpu=use_cpu)
-
-        print('=> Loading train (source) dataset')
-        trainset = []
-        for name in self.sources:
-            trainset_ = init_video_dataset(
-                name,
-                transform=self.transform_tr,
-                mode='train',
-                combineall=combineall,
-                root=root,
-                split_id=split_id,
-                seq_len=seq_len,
-                sample_method=sample_method
-            )
-            trainset.append(trainset_)
-        trainset = sum(trainset)
-
-        self._num_train_pids = trainset.num_train_pids
-        self._num_train_cams = trainset.num_train_cams
-
-        training_sampler = build_train_sampler(
-            trainset.train, train_sampler,
-            batch_size=batch_size,
-            num_instances=num_instances
-        )
-
-        self.trainloader = torch.utils.data.DataLoader(
-            trainset,
-            sampler=training_sampler,
-            batch_size=batch_size,
-            shuffle=False,
-            num_workers=workers,
-            pin_memory=self.use_gpu,
-            drop_last=True
-        )
-
-        print('=> Loading test (target) dataset')
-        self.testloader = {name: {'query': None, 'gallery': None} for name in self.targets}
-        self.testdataset = {name: {'query': None, 'gallery': None} for name in self.targets}
-
-        for name in self.targets:
-            # build query loader
-            queryset = init_video_dataset(
-                name,
-                transform=self.transform_te,
-                mode='query',
-                combineall=combineall,
-                root=root,
-                split_id=split_id,
-                seq_len=seq_len,
-                sample_method=sample_method
-            )
-            self.testloader[name]['query'] = torch.utils.data.DataLoader(
-                queryset,
-                batch_size=batch_size,
-                shuffle=False,
-                num_workers=workers,
-                pin_memory=self.use_gpu,
-                drop_last=False
-            )
-
-            # build gallery loader
-            galleryset = init_video_dataset(
-                name,
-                transform=self.transform_te,
-                mode='gallery',
-                combineall=combineall,
-                verbose=False,
-                root=root,
-                split_id=split_id,
-                seq_len=seq_len,
-                sample_method=sample_method
-            )
-            self.testloader[name]['gallery'] = torch.utils.data.DataLoader(
-                galleryset,
-                batch_size=batch_size,
-                shuffle=False,
-                num_workers=workers,
-                pin_memory=self.use_gpu,
-                drop_last=False
-            )
-
-            self.testdataset[name]['query'] = queryset.query
-            self.testdataset[name]['gallery'] = galleryset.gallery
-
-        print('\n')
-        print('  **************** Summary ****************')
-        print('  train             : {}'.format(self.sources))
-        print('  # train datasets  : {}'.format(len(self.sources)))
-        print('  # train ids       : {}'.format(self.num_train_pids))
-        print('  # train tracklets : {}'.format(len(trainset)))
-        print('  # train cameras   : {}'.format(self.num_train_cams))
-        print('  test              : {}'.format(self.targets))
-        print('  *****************************************')
-        print('\n')
+"""
+Video code (below) is deprecated
+"""
+# class VideoDataManager(DataManager):
+#     """Video data manager.
+#
+#     Args:
+#         root (str): root path to datasets.
+#         sources (str or list): source dataset(s).
+#         targets (str or list, optional): target dataset(s). If not given,
+#             it equals to ``sources``.
+#         height (int, optional): target image height. Default is 256.
+#         width (int, optional): target image width. Default is 128.
+#         random_erase (bool, optional): use random erasing. Default is False.
+#         color_jitter (bool, optional): use color jittering. Default is False.
+#         color_aug (bool, optional): use color augmentation. Default is False.
+#         use_cpu (bool, optional): use cpu. Default is False.
+#         split_id (int, optional): split id (*0-based*). Default is 0.
+#         combineall (bool, optional): combine train, query and gallery in a dataset for
+#             training. Default is False.
+#         batch_size (int, optional): number of *tracklets* in a batch. Default is 3.
+#         workers (int, optional): number of workers. Default is 4.
+#         num_instances (int, optional): number of instances per identity in a batch.
+#             Default is 4.
+#         train_sampler (str, optional): sampler. Default is empty (``RandomSampler``).
+#         seq_len (int, optional): how many images to sample in a tracklet. Default is 15.
+#         sample_method (str, optional): how to sample images in a tracklet. Default is "evenly".
+#             Choices are ["evenly", "random", "all"]. "evenly" and "random" sample ``seq_len``
+#             images in a tracklet while "all" samples all images in a tracklet, thus ``batch_size``
+#             needs to be set to 1.
+#
+#     Examples::
+#
+#         datamanager = torchreid.data.VideoDataManager(
+#             root='path/to/reid-data',
+#             sources='mars',
+#             height=256,
+#             width=128,
+#             batch_size=3,
+#             seq_len=15,
+#             sample_method='evenly'
+#         )
+#
+#     .. note::
+#         The current implementation only supports image-like training. Therefore, each image in a
+#         sampled tracklet will undergo independent transformation functions. To achieve tracklet-aware
+#         training, you need to modify the transformation functions for video reid such that each function
+#         applies the same operation to all images in a tracklet to keep consistency.
+#     """
+#
+#     def __init__(self, root='', sources=None, targets=None, height=256, width=128, random_erase=False,
+#                  color_jitter=False, color_aug=False, use_cpu=False, split_id=0, combineall=False,
+#                  batch_size=3, workers=4, num_instances=4, train_sampler=None,
+#                  seq_len=15, sample_method='evenly'):
+#
+#         super(VideoDataManager, self).__init__(sources=sources, targets=targets, height=height, width=width,
+#                                                random_erase=random_erase, color_jitter=color_jitter,
+#                                                color_aug=color_aug, use_cpu=use_cpu)
+#
+#         print('=> Loading train (source) dataset')
+#         trainset = []
+#         for name in self.sources:
+#             trainset_ = init_video_dataset(
+#                 name,
+#                 transform=self.transform_tr,
+#                 mode='train',
+#                 combineall=combineall,
+#                 root=root,
+#                 split_id=split_id,
+#                 seq_len=seq_len,
+#                 sample_method=sample_method
+#             )
+#             trainset.append(trainset_)
+#         trainset = sum(trainset)
+#
+#         self._num_train_pids = trainset.num_train_pids
+#         self._num_train_cams = trainset.num_train_cams
+#
+#         training_sampler = build_train_sampler(
+#             trainset.train, train_sampler,
+#             batch_size=batch_size,
+#             num_instances=num_instances
+#         )
+#
+#         self.trainloader = torch.utils.data.DataLoader(
+#             trainset,
+#             sampler=training_sampler,
+#             batch_size=batch_size,
+#             shuffle=False,
+#             num_workers=workers,
+#             pin_memory=self.use_gpu,
+#             drop_last=True
+#         )
+#
+#         print('=> Loading test (target) dataset')
+#         self.testloader = {name: {'query': None, 'gallery': None} for name in self.targets}
+#         self.testdataset = {name: {'query': None, 'gallery': None} for name in self.targets}
+#
+#         for name in self.targets:
+#             # build query loader
+#             queryset = init_video_dataset(
+#                 name,
+#                 transform=self.transform_te,
+#                 mode='query',
+#                 combineall=combineall,
+#                 root=root,
+#                 split_id=split_id,
+#                 seq_len=seq_len,
+#                 sample_method=sample_method
+#             )
+#             self.testloader[name]['query'] = torch.utils.data.DataLoader(
+#                 queryset,
+#                 batch_size=batch_size,
+#                 shuffle=False,
+#                 num_workers=workers,
+#                 pin_memory=self.use_gpu,
+#                 drop_last=False
+#             )
+#
+#             # build gallery loader
+#             galleryset = init_video_dataset(
+#                 name,
+#                 transform=self.transform_te,
+#                 mode='gallery',
+#                 combineall=combineall,
+#                 verbose=False,
+#                 root=root,
+#                 split_id=split_id,
+#                 seq_len=seq_len,
+#                 sample_method=sample_method
+#             )
+#             self.testloader[name]['gallery'] = torch.utils.data.DataLoader(
+#                 galleryset,
+#                 batch_size=batch_size,
+#                 shuffle=False,
+#                 num_workers=workers,
+#                 pin_memory=self.use_gpu,
+#                 drop_last=False
+#             )
+#
+#             self.testdataset[name]['query'] = queryset.query
+#             self.testdataset[name]['gallery'] = galleryset.gallery
+#
+#         print('\n')
+#         print('  **************** Summary ****************')
+#         print('  train             : {}'.format(self.sources))
+#         print('  # train datasets  : {}'.format(len(self.sources)))
+#         print('  # train ids       : {}'.format(self.num_train_pids))
+#         print('  # train tracklets : {}'.format(len(trainset)))
+#         print('  # train cameras   : {}'.format(self.num_train_cams))
+#         print('  test              : {}'.format(self.targets))
+#         print('  *****************************************')
+#         print('\n')
--- torchreid/data/datasets/dataset.py
+++ torchreid/data/datasets/dataset.py
@@ -310,80 +310,80 @@ class ImageDataset(Dataset):
         print('  ----------------------------------------')
 
 
-class VideoDataset(Dataset):
-    """A base class representing VideoDataset.
-
-    All other video datasets should subclass it.
-
-    ``__getitem__`` returns an image given index.
-    It will return ``imgs``, ``pid`` and ``camid``
-    where ``imgs`` has shape (seq_len, channel, height, width). As a result,
-    data in each batch has shape (batch_size, seq_len, channel, height, width).
-    """
-
-    def __init__(self, train, query, gallery, seq_len=15, sample_method='evenly', **kwargs):
-        super(VideoDataset, self).__init__(train, query, gallery, **kwargs)
-        self.seq_len = seq_len
-        self.sample_method = sample_method
-
-        if self.transform is None:
-            raise RuntimeError('transform must not be None')
-
-    def __getitem__(self, index):
-        img_paths, pid, camid = self.data[index]
-        num_imgs = len(img_paths)
-
-        if self.sample_method == 'random':
-            # Randomly samples seq_len images from a tracklet of length num_imgs,
-            # if num_imgs is smaller than seq_len, then replicates images
-            indices = np.arange(num_imgs)
-            replace = False if num_imgs>=self.seq_len else True
-            indices = np.random.choice(indices, size=self.seq_len, replace=replace)
-            # sort indices to keep temporal order (comment it to be order-agnostic)
-            indices = np.sort(indices)
-
-        elif self.sample_method == 'evenly':
-            # Evenly samples seq_len images from a tracklet
-            if num_imgs >= self.seq_len:
-                num_imgs -= num_imgs % self.seq_len
-                indices = np.arange(0, num_imgs, num_imgs/self.seq_len)
-            else:
-                # if num_imgs is smaller than seq_len, simply replicate the last image
-                # until the seq_len requirement is satisfied
-                indices = np.arange(0, num_imgs)
-                num_pads = self.seq_len - num_imgs
-                indices = np.concatenate([indices, np.ones(num_pads).astype(np.int32)*(num_imgs-1)])
-            assert len(indices) == self.seq_len
-
-        elif self.sample_method == 'all':
-            # Samples all images in a tracklet. batch_size must be set to 1
-            indices = np.arange(num_imgs)
-
-        else:
-            raise ValueError('Unknown sample method: {}'.format(self.sample_method))
-
-        imgs = []
-        for index in indices:
-            img_path = img_paths[int(index)]
-            img = read_image(img_path)
-            if self.transform is not None:
-                img = self.transform(img)
-            img = img.unsqueeze(0) # img must be torch.Tensor
-            imgs.append(img)
-        imgs = torch.cat(imgs, dim=0)
-
-        return imgs, pid, camid
-
-    def show_summary(self):
-        num_train_pids, num_train_cams = self.parse_data(self.train)
-        num_query_pids, num_query_cams = self.parse_data(self.query)
-        num_gallery_pids, num_gallery_cams = self.parse_data(self.gallery)
-
-        print('=> Loaded {}'.format(self.__class__.__name__))
-        print('  -------------------------------------------')
-        print('  subset   | # ids | # tracklets | # cameras')
-        print('  -------------------------------------------')
-        print('  train    | {:5d} | {:11d} | {:9d}'.format(num_train_pids, len(self.train), num_train_cams))
-        print('  query    | {:5d} | {:11d} | {:9d}'.format(num_query_pids, len(self.query), num_query_cams))
-        print('  gallery  | {:5d} | {:11d} | {:9d}'.format(num_gallery_pids, len(self.gallery), num_gallery_cams))
-        print('  -------------------------------------------')
+# class VideoDataset(Dataset):
+#     """A base class representing VideoDataset.
+#
+#     All other video datasets should subclass it.
+#
+#     ``__getitem__`` returns an image given index.
+#     It will return ``imgs``, ``pid`` and ``camid``
+#     where ``imgs`` has shape (seq_len, channel, height, width). As a result,
+#     data in each batch has shape (batch_size, seq_len, channel, height, width).
+#     """
+#
+#     def __init__(self, train, query, gallery, seq_len=15, sample_method='evenly', **kwargs):
+#         super(VideoDataset, self).__init__(train, query, gallery, **kwargs)
+#         self.seq_len = seq_len
+#         self.sample_method = sample_method
+#
+#         if self.transform is None:
+#             raise RuntimeError('transform must not be None')
+#
+#     def __getitem__(self, index):
+#         img_paths, pid, camid = self.data[index]
+#         num_imgs = len(img_paths)
+#
+#         if self.sample_method == 'random':
+#             # Randomly samples seq_len images from a tracklet of length num_imgs,
+#             # if num_imgs is smaller than seq_len, then replicates images
+#             indices = np.arange(num_imgs)
+#             replace = False if num_imgs>=self.seq_len else True
+#             indices = np.random.choice(indices, size=self.seq_len, replace=replace)
+#             # sort indices to keep temporal order (comment it to be order-agnostic)
+#             indices = np.sort(indices)
+#
+#         elif self.sample_method == 'evenly':
+#             # Evenly samples seq_len images from a tracklet
+#             if num_imgs >= self.seq_len:
+#                 num_imgs -= num_imgs % self.seq_len
+#                 indices = np.arange(0, num_imgs, num_imgs/self.seq_len)
+#             else:
+#                 # if num_imgs is smaller than seq_len, simply replicate the last image
+#                 # until the seq_len requirement is satisfied
+#                 indices = np.arange(0, num_imgs)
+#                 num_pads = self.seq_len - num_imgs
+#                 indices = np.concatenate([indices, np.ones(num_pads).astype(np.int32)*(num_imgs-1)])
+#             assert len(indices) == self.seq_len
+#
+#         elif self.sample_method == 'all':
+#             # Samples all images in a tracklet. batch_size must be set to 1
+#             indices = np.arange(num_imgs)
+#
+#         else:
+#             raise ValueError('Unknown sample method: {}'.format(self.sample_method))
+#
+#         imgs = []
+#         for index in indices:
+#             img_path = img_paths[int(index)]
+#             img = read_image(img_path)
+#             if self.transform is not None:
+#                 img = self.transform(img)
+#             img = img.unsqueeze(0) # img must be torch.Tensor
+#             imgs.append(img)
+#         imgs = torch.cat(imgs, dim=0)
+#
+#         return imgs, pid, camid
+#
+#     def show_summary(self):
+#         num_train_pids, num_train_cams = self.parse_data(self.train)
+#         num_query_pids, num_query_cams = self.parse_data(self.query)
+#         num_gallery_pids, num_gallery_cams = self.parse_data(self.gallery)
+#
+#         print('=> Loaded {}'.format(self.__class__.__name__))
+#         print('  -------------------------------------------')
+#         print('  subset   | # ids | # tracklets | # cameras')
+#         print('  -------------------------------------------')
+#         print('  train    | {:5d} | {:11d} | {:9d}'.format(num_train_pids, len(self.train), num_train_cams))
+#         print('  query    | {:5d} | {:11d} | {:9d}'.format(num_query_pids, len(self.query), num_query_cams))
+#         print('  gallery  | {:5d} | {:11d} | {:9d}'.format(num_gallery_pids, len(self.gallery), num_gallery_cams))
+#         print('  -------------------------------------------')
